698b9b4b7fc3ad11dcc2a6244d1e975e0ea9b71c
wwwck007 <329188702@qq.com>
2020-11-12 23:29:16 +0800

    Revert "Revert "gy""
    This reverts commit 421d4ec2148408933527578791aef5ed4cfa1809.

---
+++ MMMusicPlayer/app/src/main/java/Component/MyTextView.java
@@ -0,0 +1,22 @@
+package Component;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class MyTextView extends androidx.appcompat.widget.AppCompatTextView {
+    public MyTextView(Context context) {
+        super(context);
+    }
+    public MyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public MyTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+    //返回textview是否处在选中的状态
+    //而只有选中的textview才能够实现跑马灯效果
+    @Override
+    public boolean isFocused() {
+        return true;
+    }
+}
